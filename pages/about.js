59cf927efb912eed2728b68ff4b3992fbd288af5
pichid <pichid.de@hengleasing.co.th>
2020-04-19 12:01:20 +0700

    add material-ui

--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,16 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    margin: theme.spacing(6, 0, 3),
+  },
+}));
+
 function About() {
-  return <div>About</div>;
+  const classes = useStyles();
+
+  return <div className={classes.root}>About</div>;
 }
 
 export default About;
